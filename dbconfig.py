0468603fcb8e4e0382b41f44f835287944e4a0d8
Karla <karla.cantub@uanl.edu.mx>
2024-11-14 22:44:07 -0600

    :D

--- dbconfig.py
+++ dbconfig.py
@@ -2,8 +2,9 @@ import pymysql
 
 def getDBConnection():
     connection = pymysql.connect(
-        host = 'localhost',
-        user = 'root',
+        host='mysql',         
+        user='root',    
+        port=30278,
         password = 'password1',
         database = 'PIA'
     )
